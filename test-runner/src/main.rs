1adfe6e25392606cdb9ba062d2dd6ea68d569e70
torfmaster <briefe@kebes.de>
2020-02-28 16:43:26 +0100

    wip create test runner

---
+++ test-runner/src/main.rs
@@ -0,0 +1,104 @@
+use std::collections::HashMap;
+use std::process::Stdio;
+use std::time::Duration;
+use structopt::StructOpt;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::BufReader;
+use tokio::process::Command;
+use tokio::time::timeout;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "libtock test runner", about = "run libtock tests")]
+struct Arguments {
+    #[structopt(short, long)]
+    timeout_ms: usize,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    timeout(Duration::from_secs(10), perform_tests()).await?
+}
+
+async fn perform_tests() -> Result<(), Box<dyn std::error::Error>> {
+    let  tests = Command::new("qemu-system-riscv32")
+        .arg("-M")
+        .arg("sifive_e")
+        .arg("-kernel")
+        .arg("../../tock/boards/hifive1/target/riscv32imac-unknown-none-elf/release/hifive1")
+        .arg("-device")
+        .arg("loader,file=./../target/riscv32imac-unknown-none-elf/tab/riscv32/libtock_test/rv32imac.tbf,addr=0x20430000")
+        .arg("-nographic")
+        .stdout(Stdio::piped())
+        .kill_on_drop(true)
+        .spawn()?;
+
+    let stdout = tests.stdout.unwrap();
+
+    let stdout_reader = BufReader::new(stdout);
+    let mut stdout_lines = stdout_reader.lines();
+    let mut result_map = HashMap::<TockTest, bool>::new();
+
+    while let Some(line) = stdout_lines.next_line().await? {
+        if let Some(entry) = parse_line(line) {
+            result_map.insert(entry.0, entry.1);
+        }
+        if result_map.keys().len() >= 6 {
+            if result_map.values().all(|x| *x) {
+                return Ok(());
+            } else {
+                panic!();
+            }
+        }
+    }
+    panic!("qemu exited unexpectedly")
+}
+
+fn parse_line(input: String) -> Option<(TockTest, bool)> {
+    let result = input.find("[      OK ]").is_some();
+    let input = input.replace("[      OK ]", "");
+    let input = input.replace("[ FAILURE ]", "");
+    let input = input.trim();
+    println!("{}", input);
+    let tock_test = label_to_tocktest(input.to_string());
+    tock_test.map(|test| (test, result))
+}
+
+fn label_to_tocktest(label: String) -> Option<TockTest> {
+    match label.as_str() {
+        "Console" => Some(TockTest::Console),
+        "static mut" => Some(TockTest::StaticMut),
+        "Dynamic dispatch" => Some(TockTest::DynamicDispatch),
+        "Formatting" => Some(TockTest::Formatting),
+        "Heap" => Some(TockTest::Heap),
+        "Drivers only instantiable once" => Some(TockTest::DriversOnlyInstantiableOnce),
+        _ => None,
+    }
+}
+
+#[derive(PartialEq, Debug, Hash, Eq)]
+enum TockTest {
+    Console,
+    StaticMut,
+    DynamicDispatch,
+    Formatting,
+    Heap,
+    DriversOnlyInstantiableOnce,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn works() {
+        assert_eq!(
+            parse_line("[      OK ] Console".into()),
+            Some((TockTest::Console, true))
+        );
+    }
+
+    #[test]
+    pub fn fails() {
+        assert_eq!(parse_line("[      OK ]  No Console".into()), None);
+    }
+}
